1dafbe021d63aa3b6c124cb04d9cf0c849dee9c6
alageev <alageev5610@gmail.com>
2021-03-07 19:12:13 +0200

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import logo from './assets/images/logo.png';
+import { PokemonItem } from './components';
 import { Home } from './pages';
 import { GlobalStyle } from './styles/GlobalStyle';
 import { StyledApp } from './styles/StyledApp';
 
 export const App = () => (
-  <>
+  <Router>
     <GlobalStyle />
     <StyledApp>
       <img className="logo" src={logo} alt="Logo" />
-      <Home />
+      <Switch>
+        <Route exact path="/pokemon" component={Home} />
+        <Route exact path="/pokemon/:id" component={PokemonItem} />
+      </Switch>
     </StyledApp>
-  </>
+  </Router>
 );
---
+++ src/components/PokemonItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PokemonItem = () => (
+  <div>PokemonItem</div>
+);
+
+export default PokemonItem;
